191d9a7c27ea0396281cde309f8caeddf6ec74d5
Luca Panza <lupanza@redhat.com>
2024-09-10 10:42:52 +0200

    Added dry-run capability

--- notebooks_cleanup.sh
+++ notebooks_cleanup.sh
@@ -5,17 +5,27 @@
 ### This is achieved by checking the "notebooks.kubeflow.org/last-activity" annotation on the Notebook object,
 ### once gathered, we calculate the delta between that timestamp and now
 ### To stop the workbench, we annotate it with "kubeflow-resource-stopped="true""
+### 
+### If you want to issue a dry-run, execute the script with the "dry-run" parameter
+### (e.g. ./notebooks_cleanup.sh dry-run)
 ###
+
+THRESHOLD=300
 
 for notebook in `oc get notebooks.kubeflow.org --no-headers | awk '{ print $1 }'`; do
 	echo "[`date -u "+%F %T UTC"`] Checking notebook $notebook"
 	LAST_ACTIVITY=`date -d $(oc get notebooks.kubeflow.org $notebook -o jsonpath='{.metadata.annotations.notebooks\.kubeflow\.org/last-activity}') +%s`
 	NOW=`date +%s`
 	DELTA=$((NOW - LAST_ACTIVITY))
-	if [[ $DELTA -ge 300 ]]; then
-		echo "$notebook arond for more than 300s, stopping"
-		oc annotate notebooks.kubeflow.org $notebook kubeflow-resource-stopped="true"
+	if [[ $DELTA -ge $THRESHOLD ]]; then
+		echo "$notebook arond for more than ${THRESHOLD}, stopping"
+		if [[ "$1" == "dry-run" ]]; then
+			echo "This is a dry-run, not stopping $notebook"
+		else
+			oc annotate notebooks.kubeflow.org $notebook kubeflow-resource-stopped="true"
+		fi
 	else
-		echo "All good"
+		echo "All good, $notebook running for $DELTA seconds."
 	fi
+	echo
 done
